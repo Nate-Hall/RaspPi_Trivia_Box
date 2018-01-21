780d67687d4680a20bce6e863f1e3f6ec6ac034e
Unknown <nsehall.coding@gmail.com>
2018-01-21 16:56:44 +1000

    First Press Logic with arcade buttons
    Button logic is complete and prints to console.
    Still needs LED output and sound output

--- TriviaBoxController.py
+++ TriviaBoxController.py
@@ -1,45 +1,60 @@
 import pygame
 import msvcrt
+import time
+from random import randint
 
 pygame.init()
 pygame.joystick.init()
 for i in range(pygame.joystick.get_count()):
-    pygame.joystick.Joystick(i).init()
-
-lockoutDelay = 3.0
-lockoutCountdown = 0.0
-
-currentButton = -1
-currentJoy = -1
-
-lastTime = time.time()
+	pygame.joystick.Joystick(i).init()
 
 def mainLoop():
+	lockoutDelay = 2.0
+	lockoutCountdown = 0.0
+
+	currentButton = -1
+	currentJoy = -1
+
+	lastTime = time.time()
+	newTime = time.time()
+
+	playerChooser = -1
+
 	while(True):
 		if(lockoutCountdown <= 0):
 			for event in pygame.event.get(): # User did something
 				# Possible joystick actions: JOYAXISMOTION JOYBALLMOTION JOYBUTTONDOWN JOYBUTTONUP JOYHATMOTION
-		        if event.type == pygame.JOYBUTTONDOWN:
-		        	currentPressed = event.dict['button']
-		        	currentJoy = event.dict['joy']
-		        	lockoutCountdown = lockoutDelay
-
-		        	### Play appropriate sound
-		        	### Turn on light
+				if event.type == pygame.JOYBUTTONDOWN:
+					rand = randint(0, 99)
+					if(rand >= playerChooser):
+						playerChooser = rand
+						currentButton = event.dict['button']
+						currentJoy = event.dict['joy']
+			if(playerChooser > -1):
+				lockoutCountdown = lockoutDelay
+				print (str(currentJoy) + str(currentButton))
+				### Play appropriate sound
+				### Turn on light
 
 		else:
-			lockoutCountdown -= (time.time() - lastTime)
-			if(lockoutCountdown <= 0):
+			if(lockoutCountdown <= (newTime - lastTime)):
+				lockoutCountdown = 0
+				currentButton = -1
+				currentJoy = -1
+				playerChooser = -1
+				print("Reset")
 				### Turn off light
 			else:
+				lockoutCountdown -= (newTime - lastTime)
 				### Keep light on
 
 
 		if msvcrt.kbhit():
-	        key = ord(readch())
-	        if key == 27:  # escape key
-	            break
+			key = ord(msvcrt.getch())
+			if key == 27:  # escape key
+				break
 
-		lastTime = time.time()
+		lastTime = newTime
+		newTime = time.time()
 
 mainLoop()
